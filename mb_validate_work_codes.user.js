703dab02ef2ba2780d6e02da6900f57bb18b59d7
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2021-04-25 14:13:08 +0200

    [validate work codes] Add new script

---
+++ mb_validate_work_codes.user.js
@@ -0,0 +1,119 @@
+// ==UserScript==
+// @name         MB: Validate Work Codes
+// @version      2021.4.25
+// @description  Validate work identifier codes: Highlight invalid or ill-formatted work codes.
+// @author       ROpdebee
+// @license      MIT; https://opensource.org/licenses/MIT
+// @namespace    https://github.com/ROpdebee/mb-userscripts
+// @downloadURL  https://raw.github.com/ROpdebee/mb-userscripts/main/mb_validate_work_codes.user.js
+// @updateURL    https://raw.github.com/ROpdebee/mb-userscripts/main/mb_validate_work_codes.user.js
+// @match        *://*.musicbrainz.org/work/*
+// @match        *://musicbrainz.org/work/*
+// @match        *://*.musicbrainz.org/artist/*/works
+// @match        *://musicbrainz.org/artist/*/works
+// @match        *://musicbrainz.org/edit/*
+// @match        *://*.musicbrainz.org/edit/*
+// @match        *://musicbrainz.org/*/edits*
+// @match        *://*.musicbrainz.org/*/edits*
+// @match        *://musicbrainz.org/collection/*
+// @match        *://*.musicbrainz.org/collection/*
+// @exclude      *://*.musicbrainz.org/work/*/edit
+// @exclude      *://musicbrainz.org/work/*/edit
+// @require      https://code.jquery.com/jquery-3.6.0.min.js
+// @require      https://raw.github.com/ROpdebee/mb-userscripts/main/lib/work_identifiers.js
+// @run-at       document-end
+// ==/UserScript==
+
+// A bit too similar to mb_collapse_work_attributes to my liking, but eh.
+
+let $ = this.$ = this.jQuery = jQuery.noConflict(true);
+
+const ATTR_TRANSLATIONS = [
+    'Attributes',
+    'Eigenschaften',
+    'Attributs',
+    'Attributi',
+    'Eigenschappen'];
+
+
+function highlightElement(el, level, title) {
+    let color = level === 'error' ? 'FireBrick' : 'Orange';
+    el.style.color = color;
+    el.style.fontWeight = 'bold';
+    if (title) {
+        el.title = title;
+        el.style.textDecoration = 'underline dotted 2px';
+    }
+}
+
+function checkElement(el, code, agencyId) {
+    let result = MBWorkIdentifiers.validateCode(code, agencyId);
+    if (!result.isValid) {
+        highlightElement(el, 'error', result.message);
+    } else if (result.wasChanged) {
+        highlightElement(el, 'warning', result.formattedCode);
+    }
+}
+
+function processTabulatedPage() {
+    document.querySelectorAll('table.tbl').forEach(processTable);
+}
+
+function processTable(table) {
+    let columnIdx = 1 + [...table.querySelectorAll('thead th')]
+        .findIndex(th => ATTR_TRANSLATIONS.includes(th.innerText));
+    let attrLis = table.querySelectorAll('td:nth-child(' + columnIdx + ') li');
+
+    attrLis.forEach(el => {
+        let match = /(.+)\s\((.+?)\)/.exec(el.innerText);
+        let text, code, agencyId;
+        try {
+            [text, code, agencyId] = match;
+        } catch {
+            return;
+        }
+
+        checkElement(el, code, agencyId);
+    });
+}
+
+function processWorkPage() {
+    let attrs = $('dl.properties > dd.work-attribute');
+
+    attrs.each((i, el) => {
+        let agencyDD = el.previousSibling();
+        let agencyId = agencyId.innerText.slice(0, -1);
+
+        checkElement(el, el.innerText, agencyId);
+    });
+}
+
+function processEditPage() {
+    $('table.details.edit-work, table.details.add-work').each((i, tbl) => {
+        let attrRows = $(tbl)
+            .find('tr')
+            .filter((i, tr) => {
+                let attrName = tr.querySelector('th').innerText;
+                return attrName.endsWith(' ID:') || attrName === 'PRS tune code:';
+            });
+
+        attrRows.each((i, row) => {
+            let agencyId = row.querySelector('th').innerText.slice(0, -1);
+            row.querySelectorAll('li > span').forEach(el => {
+                checkElement(el, el.innerText, agencyId);
+            });
+        });
+    });
+
+    $('table.details.merge-works').each((i, tbl) => {
+        tbl.querySelectorAll('table.tbl').forEach(processTable);
+    });
+}
+
+if ((location.pathname.startsWith('/artist/') || location.pathname.startsWith('/collection/')) && location.pathname.split('/')[3] !== 'edits') {
+    processTabulatedPage();
+} else if (location.pathname.startsWith('/work/') && location.pathname.split('/')[3] !== 'edits') {
+    processWorkPage();
+} else {
+    processEditPage();
+}
