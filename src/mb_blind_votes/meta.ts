c52bb9c04d2655f5ff3e074d6267fc2c3bef3b60
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2021-06-01 02:25:30 +0200

    Remove unused jQuery requirement

--- src/mb_blind_votes/meta.ts
+++ src/mb_blind_votes/meta.ts
@@ -7,8 +7,5 @@ export default {
     version: '2021.3.30',
     description: 'Blinds editor details before your votes are cast.',
     'run-at': 'document-body',
-    require: [
-        'https://code.jquery.com/jquery-3.6.0.min.js',
-    ],
     match: transformMBRequires('*'),
 } as UserscriptMetadata;
