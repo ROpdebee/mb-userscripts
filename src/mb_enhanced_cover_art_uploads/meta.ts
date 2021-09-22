ef5deb4ba98552dec8154310c75f0dd49df17d5b
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2021-09-22 14:18:45 +0200

    [caa upload] Release 2021.9.22.3

--- src/mb_enhanced_cover_art_uploads/meta.ts
+++ src/mb_enhanced_cover_art_uploads/meta.ts
@@ -4,7 +4,7 @@ import { transformMBMatchURL } from '../lib/util/metadata.js';
 
 const metadata: UserscriptMetadata = {
     name: 'MB: Enhanced Cover Art Uploads',
-    version: '2021.9.22.2',
+    version: '2021.9.22.3',
     description: 'Enhance the cover art uploader! Upload directly from a URL, automatically import covers from Discogs/Spotify/Apple Music/..., automatically retrieve the largest version, and more!',
     'run-at': 'document-load',
     match: [
