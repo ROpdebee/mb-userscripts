0e445799b93df2c8d9e38f479117d3390c84c562
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2021-09-22 13:18:26 +0200

    [caa upload] Release 2021.9.22

--- dist/mb_enhanced_cover_art_uploads.meta.js
+++ dist/mb_enhanced_cover_art_uploads.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MB: Enhanced Cover Art Uploads
 // @description  Enhance the cover art uploader! Upload directly from a URL, automatically import covers from Discogs/Spotify/Apple Music/..., automatically retrieve the largest version, and more!
-// @version      2021.9.21
+// @version      2021.9.22
 // @author       ROpdebee
 // @license      MIT; https://opensource.org/licenses/MIT
 // @namespace    https://github.com/ROpdebee/mb-userscripts
@@ -12,7 +12,7 @@
 // @match        *://*.musicbrainz.org/release/*/add-cover-art
 // @match        *://atisket.pulsewidth.org.uk/*
 // @exclude      *://atisket.pulsewidth.org.uk/
-// @require      https://github.com/qsniyg/maxurl/blob/master/userscript.user.js?raw=true
+// @require      https://github.com/qsniyg/maxurl/blob/563626fe3b7c5ed3f6dc19d90a356746c68b5b4b/userscript.user.js?raw=true
 // @run-at       document-load
 // @grant        GM_xmlhttpRequest
 // @grant        GM_getValue
