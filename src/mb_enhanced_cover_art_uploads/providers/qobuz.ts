58a69c4d347ffa37c1099a253f878e1cff55d0a6
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-09-26 13:11:59 +0200

    [caa upload] Support open.qobuz.com

--- src/mb_enhanced_cover_art_uploads/providers/qobuz.ts
+++ src/mb_enhanced_cover_art_uploads/providers/qobuz.ts
@@ -3,19 +3,39 @@ import { assertHasValue } from '../../lib/util/assert';
 import type { CoverArt, CoverArtProvider } from './base';
 import { ArtworkTypeIDs } from './base';
 
-const ID_MATCH_REGEX = /\/album\/[^/]+\/([A-Za-z0-9]+)(?:\/|$)/;
+// Splitting these regexps up for each domain. www.qobuz.com includes the album
+// title in the URL, open.qobuz.com does not. Although we could make the album
+// title part optional and match both domains with the same regexp, this could
+// lead to issues with URLs like this:
+// https://open.qobuz.com/album/1234567890/related
+// Not sure if such URLs would ever occur, but using a single regexp could
+// lead to `related` being matched as the ID and the actual ID as the title.
+const WWW_ID_MATCH_REGEX = /\/album\/[^/]+\/([A-Za-z0-9]+)(?:\/|$)/;
+const OPEN_ID_MATCH_REGEX = /\/album\/([A-Za-z0-9]+)(?:\/|$)/;
 
 export class QobuzProvider implements CoverArtProvider {
-    supportedDomains = ['qobuz.com']
+    supportedDomains = ['qobuz.com', 'open.qobuz.com']
     favicon = 'https://www.qobuz.com/favicon.ico'
     name = 'Qobuz'
 
     supportsUrl(url: URL): boolean {
-        return ID_MATCH_REGEX.test(url.pathname);
+        if (url.hostname === 'open.qobuz.com') {
+            return OPEN_ID_MATCH_REGEX.test(url.pathname);
+        }
+
+        return WWW_ID_MATCH_REGEX.test(url.pathname);
+    }
+
+    static extractId(url: URL): string {
+        if (url.hostname === 'open.qobuz.com') {
+            return url.pathname.match(OPEN_ID_MATCH_REGEX)?.[1];
+        }
+
+        return url.pathname.match(WWW_ID_MATCH_REGEX)?.[1];
     }
 
     async findImages(url: URL): Promise<CoverArt[]> {
-        const id = url.pathname.match(ID_MATCH_REGEX)?.[1];
+        const id = QobuzProvider.extractId(url);
         assertHasValue(id);
         const d1 = id.substring(id.length - 2);
         const d2 = id.substring(id.length - 4, id.length - 2);
