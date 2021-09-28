60110951ef9413d31d451b2bc2442381d6d592cb
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-09-28 23:23:59 +0200

    [tests] Add tests for array helpers

---
+++ tests/lib/util/array.test.ts
@@ -0,0 +1,33 @@
+import { filterNonNull, findRight } from "@lib/util/array";
+
+describe('filtering null values', () => {
+    it('retains non-null values', () => {
+        expect(filterNonNull([1, 2, 3])).toStrictEqual([1, 2, 3]);
+    });
+
+    it('filters out null values', () => {
+        expect(filterNonNull([1, null, 2])).toStrictEqual([1, 2]);
+    });
+
+    it('filters out multiple null values', () => {
+        expect(filterNonNull([1, null, 2, null])).toStrictEqual([1, 2]);
+    });
+
+    it('returns empty array when only null', () => {
+        expect(filterNonNull([null, null])).toStrictEqual([]);
+    });
+});
+
+describe('finding from right', () => {
+    it('finds rightmost element matching predicate', () => {
+        expect(findRight([1, 2, 3], (i) => i < 5)).toBe(3);
+    });
+
+    it('finds rightmost element matching predicate, if it is first element', () => {
+        expect(findRight([1, 2, 3], (i) => i <= 1)).toBe(1);
+    });
+
+    it('returns null when no item matches', () => {
+        expect(findRight([1, 2, 3], (i) => i > 5)).toBeNull();
+    });
+});
