7933a22d986ef98b444690b2efa9f3a650f6584b
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-09-28 21:18:10 +0200

    [tests] Adapt `xhr` tests after #69

--- tests/lib/util/xhr.test.ts
+++ tests/lib/util/xhr.test.ts
@@ -1,4 +1,4 @@
-import { gmxhr } from '@lib/util/xhr';
+import { AbortedError, gmxhr, HTTPResponseError, NetworkError, TimeoutError } from '@lib/util/xhr';
 
 let mocked_GM_xhr = jest.fn() as jest.MockedFunction<typeof GM_xmlhttpRequest>;
 global.GM_xmlhttpRequest = mocked_GM_xhr;
@@ -16,30 +16,61 @@ describe('gmxhr', () => {
 
     it('resolves on load', async () => {
         mocked_GM_xhr.mockImplementation((options) => options.onload?.(stubResponse));
-        await expect(gmxhr({ url: 'test', method: 'GET' })).resolves.toBe(stubResponse);
+        await expect(gmxhr('test')).resolves.toBe(stubResponse);
     });
 
     it('rejects on error', async () => {
         mocked_GM_xhr.mockImplementation((options) => options.onerror?.(stubResponse));
-        await expect(gmxhr({ url: 'test', method: 'GET'})).rejects.toEqual({'reason': 'network error', 'error': stubResponse});
+        await expect(gmxhr('test')).rejects.toBeInstanceOf(NetworkError);
     });
 
     it('rejects on timeout', async () => {
         mocked_GM_xhr.mockImplementation((options) => options.ontimeout?.(stubResponse));
-        await expect(gmxhr({ url: 'test', method: 'GET'})).rejects.toEqual({'reason': 'timed out', 'error': stubResponse});
+        await expect(gmxhr('test')).rejects.toBeInstanceOf(TimeoutError);
     });
 
     it('rejects on abort', async () => {
         mocked_GM_xhr.mockImplementation((options) => options.onabort?.(stubResponse));
-        await expect(gmxhr({ url: 'test', method: 'GET'})).rejects.toEqual({'reason': 'aborted', 'error': stubResponse});
+        await expect(gmxhr('test')).rejects.toBeInstanceOf(AbortedError);
     });
 
     it('rejects on HTTP error', async () => {
+        expect.assertions(2);
         mocked_GM_xhr.mockImplementation((options) => options.onload?.({
             status: 400,
             statusText: 'Bad request',
         } as unknown as GMXMLHttpRequestResponse));
-        await expect(gmxhr({ url: 'test', method: 'GET'})).rejects.toEqual({'reason': 'HTTP error Bad request', 'error': { status: 400, statusText: 'Bad request'}});
+        const res = gmxhr('test');
+        await expect(res).rejects.toBeInstanceOf(HTTPResponseError);
+        await expect(res).rejects.toMatchObject({ statusCode: 400, statusText: 'Bad request'});
+    });
+
+    it('uses GET by default', async () => {
+        mocked_GM_xhr.mockImplementation((options) => options.onload?.(stubResponse));
+        await gmxhr('test');
+        expect(mocked_GM_xhr).toBeCalledTimes(1);
+        expect(mocked_GM_xhr).toBeCalledWith(expect.objectContaining({ method: 'GET' }));
+    });
+
+    it('allows overriding HTTP method', async () => {
+        mocked_GM_xhr.mockImplementation((options) => options.onload?.(stubResponse));
+        await gmxhr('test', { method: 'POST' });
+        expect(mocked_GM_xhr).toBeCalledTimes(1);
+        expect(mocked_GM_xhr).toBeCalledWith(expect.objectContaining({ method: 'POST' }));
+    });
+
+    it('accepts string URLs', async () => {
+        mocked_GM_xhr.mockImplementation((options) => options.onload?.(stubResponse));
+        await gmxhr('https://example.com/test?x=1#y');
+        expect(mocked_GM_xhr).toBeCalledTimes(1);
+        expect(mocked_GM_xhr).toBeCalledWith(expect.objectContaining({ url: 'https://example.com/test?x=1#y' }));
+    });
+
+    it('accepts URL URLs', async () => {
+        mocked_GM_xhr.mockImplementation((options) => options.onload?.(stubResponse));
+        await gmxhr(new URL('https://example.com/test?x=1#y'));
+        expect(mocked_GM_xhr).toBeCalledTimes(1);
+        expect(mocked_GM_xhr).toBeCalledWith(expect.objectContaining({ url: 'https://example.com/test?x=1#y' }));
     });
 
 });
