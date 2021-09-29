adb7f60e2225095dbbde66945ec22c95207c7c55
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-09-29 19:15:24 +0200

    Make linting more strict

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -49,34 +49,34 @@ module.exports = {
                 project: './tsconfig.json',
             },
             rules: {
-                '@typescript-eslint/array-type': ['warn', {
+                '@typescript-eslint/array-type': ['error', {
                     default: 'array-simple',
                 }],
-                '@typescript-eslint/consistent-indexed-object-style': ['warn', 'record'],
-                '@typescript-eslint/consistent-type-assertions': ['warn', {
+                '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+                '@typescript-eslint/consistent-type-assertions': ['error', {
                     assertionStyle: 'as',
                     objectLiteralTypeAssertions: 'allow-as-parameter',
                 }],
-                '@typescript-eslint/consistent-type-imports': ['warn', {
+                '@typescript-eslint/consistent-type-imports': ['error', {
                     prefer: 'type-imports',
                     disallowTypeAnnotations: true,
                 }],
-                '@typescript-eslint/explicit-function-return-type': ['warn'],
-                '@typescript-eslint/member-delimiter-style': ['warn', {
+                '@typescript-eslint/explicit-function-return-type': ['error'],
+                '@typescript-eslint/member-delimiter-style': ['error', {
                     multiline: {
                         delimiter: 'none',
                     },
                 }],
-                '@typescript-eslint/no-base-to-string': ['warn'],
-                '@typescript-eslint/no-confusing-void-expression': ['warn'],
-                '@typescript-eslint/no-invalid-void-type': ['warn'],
-                '@typescript-eslint/no-unnecessary-condition': ['warn'],
-                '@typescript-eslint/no-unsafe-argument': ['warn'],
-                '@typescript-eslint/prefer-nullish-coalescing': ['warn'],
-                '@typescript-eslint/prefer-optional-chain': ['warn'],
-                '@typescript-eslint/prefer-reduce-type-parameter': ['warn'],
-                '@typescript-eslint/prefer-ts-expect-error': ['warn'],
-                '@typescript-eslint/type-annotation-spacing': ['warn'],
+                '@typescript-eslint/no-base-to-string': ['error'],
+                '@typescript-eslint/no-confusing-void-expression': ['error'],
+                '@typescript-eslint/no-invalid-void-type': ['error'],
+                '@typescript-eslint/no-unnecessary-condition': ['error'],
+                '@typescript-eslint/no-unsafe-argument': ['error'],
+                '@typescript-eslint/prefer-nullish-coalescing': ['error'],
+                '@typescript-eslint/prefer-optional-chain': ['error'],
+                '@typescript-eslint/prefer-reduce-type-parameter': ['error'],
+                '@typescript-eslint/prefer-ts-expect-error': ['error'],
+                '@typescript-eslint/type-annotation-spacing': ['error'],
             }
         }, { // Override per eslint-plugin-jest documentation.
             files: ['tests/**'],
