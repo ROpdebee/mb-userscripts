6b930278523e21fbd0e0547af4f08db766997a6e
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-01 16:39:15 +0200

    [tests] Add tests for Deezer provider

---
+++ tests/mb_enhanced_cover_art_uploads/providers/deezer.test.ts
@@ -0,0 +1,46 @@
+import { setupPolly } from '@test-utils/pollyjs';
+
+import { ArtworkTypeIDs } from '@src/mb_enhanced_cover_art_uploads/providers/base';
+import { DeezerProvider } from '@src/mb_enhanced_cover_art_uploads/providers/deezer';
+
+describe('deezer provider', () => {
+    const pollyContext = setupPolly();
+    const provider = new DeezerProvider();
+
+    it.each`
+        url | desc
+        ${'https://www.deezer.com/album/260364202'} | ${'with clean URL'}
+        ${'https://www.deezer.com/en/album/215928292?deferredFl=1'} | ${'with dirty URL'}
+        ${'https://www.deezer.com/en/album/215928292'} | ${'with language'}
+    `('matches album links $desc', ({ url }: { url: string }) => {
+        expect(provider.supportsUrl(new URL(url)))
+            .toBeTrue();
+    });
+
+    it.each`
+        url | type
+        ${'https://www.deezer.com/en/artist/4023815'} | ${'artist'}
+        ${'https://www.deezer.com/en/track/1500277672'} | ${'track'}
+    `('does not match $type links', ({ url }: { url: string }) => {
+        expect(provider.supportsUrl(new URL(url)))
+            .toBeFalse();
+    });
+
+    it('grabs the correct cover', async () => {
+        const coverUrl = await provider.findImages(new URL('https://www.deezer.com/en/album/260364202'));
+
+        expect(coverUrl).toBeArrayOfSize(1);
+        expect(coverUrl[0].url.pathname).toContain('4097bb08bad497561ef60ec53162351e');
+        expect(coverUrl[0].type).toStrictEqual([ArtworkTypeIDs.Front]);
+        expect(coverUrl[0].comment).toBeUndefined();
+    });
+
+    it('throws if release does not exist', async () => {
+        pollyContext.polly.configure({
+            recordFailedRequests: true
+        });
+
+        await expect(provider.findImages(new URL('https://www.deezer.com/en/album/1')))
+            .toReject();
+    });
+});
