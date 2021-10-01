b71932c5ed1e955bc6fed70a74c4896becb5ee45
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-01 20:01:08 +0200

    [caa upload] Test and improve Tidal provider
    * Use Tidal API instead of requesting the page, since Tidal is
      apparently very aggressive in IP banning on automated requests.

--- src/mb_enhanced_cover_art_uploads/providers/tidal.ts
+++ src/mb_enhanced_cover_art_uploads/providers/tidal.ts
@@ -1,35 +1,68 @@
 import { assertHasValue } from '@lib/util/assert';
-import { parseDOM, qs, qsMaybe } from '@lib/util/dom';
 import { gmxhr } from '@lib/util/xhr';
 
 import type { CoverArt, CoverArtProvider } from './base';
 import { ArtworkTypeIDs } from './base';
+
+// Extracted from listen.tidal.com JS. There seem to be at least 10 different
+// API keys, I guess they might depend on the user type and/or country?
+// Also not sure whether these may change often or not. If they do, we might
+// need to switch to extracting them from the JS.
+// However, seeing as this key has been present in their JS code for at least
+// 3 years already, I doubt this will stop working any time soon.
+// https://web.archive.org/web/20181015184006/https://listen.tidal.com/app.9dbb572e8121f8755b73.js
+const APP_ID = 'CzET4vdadNUFQ5JU';
 
 export class TidalProvider implements CoverArtProvider {
     supportedDomains = ['tidal.com', 'listen.tidal.com', 'store.tidal.com']
     favicon = 'https://listen.tidal.com/favicon.ico'
     name = 'Tidal'
 
+    #countryCode: string | null = null;
+
     supportsUrl(url: URL): boolean {
         return /\/album\/\d+/.test(url.pathname);
     }
 
+    async getCountryCode(): Promise<string> {
+        if (!this.#countryCode) {
+            const resp = await gmxhr('https://listen.tidal.com/v1/country/context?countryCode=WW&locale=en_US&deviceType=BROWSER', {
+                headers: {
+                    'x-tidal-token': APP_ID,
+                },
+            });
+            this.#countryCode = JSON.parse(resp.responseText).countryCode;
+        }
+        assertHasValue(this.#countryCode, 'Cannot determine Tidal country');
+        return this.#countryCode;
+    }
+
+    async getCoverUrlFromMetadata(albumId: string): Promise<string> {
+        const countryCode = await this.getCountryCode();
+        // Not sure whether it's strictly necessary to ping, but let's impersonate
+        // the browser as much as we can to avoid getting accidentally blocked.
+        await gmxhr('https://listen.tidal.com/v1/ping');
+        const apiUrl = `https://listen.tidal.com/v1/pages/album?albumId=${albumId}&countryCode=${countryCode}&deviceType=BROWSER`;
+        const resp = await gmxhr(apiUrl, {
+            headers: {
+                'x-tidal-token': APP_ID,
+            },
+        });
+        const metadata = JSON.parse(resp.responseText);
+        const coverId = metadata?.rows?.[0]?.modules?.[0]?.album?.cover;
+        assertHasValue(coverId, 'Could not find cover in Tidal metadata');
+        return `https://resources.tidal.com/images/${coverId.replace(/-/g, '/')}/origin.jpg`;
+    }
+
     async findImages(url: URL): Promise<CoverArt[]> {
         // Rewrite the URL to point to the main page
+        // Both listen.tidal.com and store.tidal.com load metadata through an
+        // API. Bare tidal.com returns the image in a <meta> property.
         const albumId = url.pathname.match(/\/album\/(\d+)/)?.[1];
         assertHasValue(albumId);
-        url.href = `https://tidal.com/browse/album/${albumId}`;
-
-        const resp = await gmxhr(url);
-        const respDocument = parseDOM(resp.responseText);
-
-        if (qsMaybe('p#cmsg') !== null) {
-            throw new Error('Tidal presented us with a captcha');
-        }
-
-        const coverElmt = qs<HTMLMetaElement>('head > meta[property="og:image"]', respDocument);
+        const coverUrl = await this.getCoverUrlFromMetadata(albumId);
         return [{
-            url: new URL(coverElmt.content),
+            url: new URL(coverUrl),
             type: [ArtworkTypeIDs.Front],
         }];
     }
