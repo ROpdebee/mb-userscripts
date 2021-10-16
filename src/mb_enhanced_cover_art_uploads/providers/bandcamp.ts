95ca98b8dd14681406b25942b725c4124bf3f662
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-16 19:01:26 +0200

    fix(caa upload): reject redirected Bandcamp releases

--- src/mb_enhanced_cover_art_uploads/providers/bandcamp.ts
+++ src/mb_enhanced_cover_art_uploads/providers/bandcamp.ts
@@ -1,11 +1,17 @@
 import { HeadMetaPropertyProvider } from './base';
 
+const ID_REGEX = /:\/\/(.+)\.bandcamp\.com\/(track|album)\/([^/]+)(?:\/|$)/;
+
 export class BandcampProvider extends HeadMetaPropertyProvider {
     supportedDomains = ['bandcamp.com']
     favicon = 'https://s4.bcbits.com/img/favicon/favicon-32x32.png'
     name = 'Bandcamp'
 
     supportsUrl(url: URL): boolean {
-        return /bandcamp\.com\/(?:track|album)\//.test(url.href);
+        return ID_REGEX.test(url.href);
+    }
+
+    extractId(url: URL): string | undefined {
+        return url.href.match(ID_REGEX)?.slice(1)?.join('/');
     }
 }
