306865452fe5831156c64a39e6b61c85a91a1884
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-16 17:10:05 +0200

    test: remove broken pollyjs adapter test
    This test will always fail if we run the tests in passthrough mode.
    It also doesn't really serve a purpose anymore, as many of our tests
    will fail if the adapter stops working anyway.

--- tests/utils/gmxhr-adapter.test.ts
+++ tests/utils/gmxhr-adapter.test.ts
@@ -5,7 +5,7 @@ import FSPersister from '@pollyjs/persister-fs';
 import GMXHRAdapter from './gmxhr-adapter';
 
 describe('gmxhr adapter', () => {
-    const context = setupPolly({
+    setupPolly({
         adapters: [GMXHRAdapter],
         recordIfMissing: true,
         persister: FSPersister,
@@ -27,20 +27,5 @@ describe('gmxhr adapter', () => {
 
         expect(resp.status).toBe(200);
         expect(JSON.parse(resp.responseText).id).toBe(1);
-    });
-
-    it('should reuse recordings', async () => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        const requester = jest.spyOn(context.polly.adapters.get('GM_xmlhttpRequest')!, 'passthroughRequest');
-
-        await new Promise<GMXMLHttpRequestResponse>((resolve) => {
-            GM_xmlhttpRequest({
-                url: 'https://jsonplaceholder.typicode.com/posts/1',
-                method: 'GET',
-                onload: resolve,
-            });
-        });
-
-        expect(requester).not.toHaveBeenCalled();
     });
 });
