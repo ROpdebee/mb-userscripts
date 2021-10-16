1551ce6192f96f11aeadb6dd7f91697d9a0fef5f
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-16 19:01:49 +0200

    fix(caa upload): reject redirected Deezer releases

--- src/mb_enhanced_cover_art_uploads/providers/deezer.ts
+++ src/mb_enhanced_cover_art_uploads/providers/deezer.ts
@@ -1,11 +1,17 @@
 import { HeadMetaPropertyProvider } from './base';
 
+const ID_REGEX = /(?:\w{2}\/)?album\/(\d+)/;
+
 export class DeezerProvider extends HeadMetaPropertyProvider {
     supportedDomains = ['deezer.com']
     favicon = 'https://e-cdns-files.dzcdn.net/cache/images/common/favicon/favicon-16x16.526cde4edf20647be4ee32cdf35c1c13.png'
     name = 'Deezer'
 
     supportsUrl(url: URL): boolean {
-        return /(?:\w{2}\/)?album\/\d+/.test(url.pathname);
+        return ID_REGEX.test(url.pathname);
+    }
+
+    extractId(url: URL): string | undefined {
+        return url.pathname.match(ID_REGEX)?.[1];
     }
 }
