8f1b76afe7571de771fbb95f2bd22a73a3a833eb
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-17 01:01:09 +0200

    refactor(caa upload): use classes instead of inline styles

--- src/mb_enhanced_cover_art_uploads/ui/status_banner.tsx
+++ src/mb_enhanced_cover_art_uploads/ui/status_banner.tsx
@@ -1,11 +1,18 @@
 import type { LoggingSink } from '@lib/logging/sink';
 
+type BannerMessageClass = 'info' | 'warning' | 'error' | 'success';
+
 export class StatusBanner implements LoggingSink {
     #banner: HTMLSpanElement;
+    // Current banner message's class name. We need to remember this so we can
+    // clear it later on to replace it by another.
+    #currentMessageClass: BannerMessageClass = 'info';
 
     constructor() {
         this.#banner = <span
             id='ROpdebee_paste_url_status'
+            // Use info as default, it'll be overwritten on the first message.
+            className='info'
             style={{ display: 'none' }}
         />;
     }
@@ -19,24 +26,32 @@ export class StatusBanner implements LoggingSink {
         this.#banner.style.removeProperty('display');
     }
 
+    #setStatusBannerClass(newClass: BannerMessageClass): void {
+        // Need to take care to only remove the previous class that indicates
+        // the message type. Although we're (at the time of writing) not using
+        // any other classes on this element, we may do so in the future.
+        this.#banner.classList.replace(this.#currentMessageClass, newClass);
+        this.#currentMessageClass = newClass;
+    }
+
     onInfo(message: string): void {
         this.#setStatusBanner(message);
-        this.#banner.style.color = 'black';
+        this.#setStatusBannerClass('info');
     }
 
     onWarn(message: string): void {
         this.#setStatusBanner(message);
-        this.#banner.style.color = 'orange';
+        this.#setStatusBannerClass('warning');
     }
 
     onError(message: string, exception?: unknown): void {
         this.#setStatusBanner(message, exception);
-        this.#banner.style.color = 'red';
+        this.#setStatusBannerClass('error');
     }
 
     onSuccess(message: string): void {
         this.#setStatusBanner(message);
-        this.#banner.style.color = 'green';
+        this.#setStatusBannerClass('success');
     }
 
     get htmlElement(): HTMLSpanElement {
