189b0426af39271b0c408746e558ba8caafb2349
David Kellner <52860029+kellnerd@users.noreply.github.com>
2021-10-17 23:39:22 +0200

    feat(caa upload): extract images from Amazon's embedded JavaScript
    Using the thumbnails which are shown on the page has two disadvantages:
    - Pages show only a limited number of thumbnails, not all of them.
    - Maximised versions of these might still not be the largest available.
    
    Unfortunately the most reliable way (so far) to get all images in their
    highest resolution, is to extract an object from the page's embedded
    JavaScript. The advantage of this approach is that we can also extract
    artwork types from there now (at least 'Front' and 'Back', maybe others).

--- src/mb_enhanced_cover_art_uploads/providers/amazon.ts
+++ src/mb_enhanced_cover_art_uploads/providers/amazon.ts
@@ -5,6 +5,21 @@ import { ArtworkTypeIDs, CoverArtProvider } from './base';
 
 const PLACEHOLDER_IMG_REGEX = /01RmK(?:\+|%2B)J4pJL/;
 
+// Incomplete, only what we need
+interface AmazonImage {
+    hiRes: string | null // URL of the largest version, can still be maximised by IMU
+    thumb: string // this kind of URL can also be extracted from the DOM
+    large: string // maximised version of `thumb`, can not be further maximised by IMU
+    variant: string // see mapping below
+}
+
+const VARIANT_TYPE_MAPPING: Record<string, ArtworkTypeIDs | undefined> = {
+    MAIN: ArtworkTypeIDs.Front,
+    BACK: ArtworkTypeIDs.Back,
+    // PT01: ArtworkTypeIDs.Other,
+    // PT02... Are these additional images always photos or renderings of the packaging?
+};
+
 export class AmazonProvider extends CoverArtProvider {
     supportedDomains = [
         'amazon.ca', 'amazon.cn', 'amazon.de', 'amazon.es', 'amazon.fr',
@@ -27,18 +42,35 @@ export class AmazonProvider extends CoverArtProvider {
             return this.#extractFromStreamingProduct(pageDom);
         }
 
-        // Thumbnails in the sidebar, IMU will maximise
-        const imgs = qsa<HTMLImageElement>('#altImages img', pageDom);
-        const covers: CoverArt[] = imgs
-            // Filter out placeholder images.
-            .filter((img) => !PLACEHOLDER_IMG_REGEX.test(img.src))
-            .map((img) => {
-                return { url: new URL(img.src) };
-            });
+        // eslint-disable-next-line init-declarations
+        let covers: CoverArt[];
 
-        // We don't know anything about the types of these images, but we can
-        // probably assume the first image is the front cover.
-        if (covers.length) {
+        const embeddedImages = page.match(/^'colorImages': { 'initial': (.+)},$/m)?.[1];
+        if (embeddedImages) {
+            // Found image sources in the page's embedded JavaScript
+            const imgs = JSON.parse(embeddedImages) as AmazonImage[];
+            covers = imgs.map((img) => {
+                // `img.hiRes` is probably only `null` when `img.large` is the placeholder image?
+                const url = new URL(img.hiRes ?? img.large);
+                const type = VARIANT_TYPE_MAPPING[img.variant];
+                if (type) {
+                    return { url, types: [type] };
+                }
+                return { url };
+            });
+        } else {
+            // Thumbnails in the sidebar, IMU will maximise (but it might not be the highest resolution available)
+            const imgs = qsa<HTMLImageElement>('#altImages img', pageDom);
+            covers = imgs.map((img) => ({ url: new URL(img.src) }));
+        }
+
+        // Filter out placeholder images.
+        covers = covers.filter((img) => !PLACEHOLDER_IMG_REGEX.test(img.url.href));
+
+        // We don't know anything about the types of these images if they are
+        // from the thumbnail sidebar, but we can probably assume the first
+        // image is the front cover if its type has not been set already.
+        if (covers.length && covers[0].types === undefined) {
             covers[0].types = [ArtworkTypeIDs.Front];
         }
 
