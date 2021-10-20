9287205a14e76d647c1104b3fcba2a3db830736b
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2021-10-20 18:58:17 +0200

    ci: testing deployment preview
    Dummy change to test and experiment with the deployment preview

--- src/lib/util/assert.ts
+++ src/lib/util/assert.ts
@@ -12,7 +12,7 @@ export class AssertionError extends Error {}
  */
 export function assert(condition: boolean, message?: string): asserts condition {
     if (!condition) {
-        throw new AssertionError(message ?? 'Assertion failed');
+        throw new AssertionError(message ?? 'Assertion failed!!!');
     }
 }
 
