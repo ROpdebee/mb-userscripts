5bc5871d49da41c5443ef09498f07c65a04dadb8
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-20 16:18:08 +0200

    ci: another dummy change to trigger rebuild

--- src/lib/util/assert.ts
+++ src/lib/util/assert.ts
@@ -23,7 +23,7 @@ export function assert(condition: boolean, message?: string): asserts condition 
  * @param      {string}         message  The message
  */
 export function assertDefined<T>(value: T, message?: string): asserts value is Exclude<T, undefined> {
-    assert(typeof value !== 'undefined', message ?? 'Assertion failed: Expected value to be defined');
+    assert(typeof value !== 'undefined', message ?? 'Assertion failed: Expected value to be defined!!!!!');
 }
 
 /**
