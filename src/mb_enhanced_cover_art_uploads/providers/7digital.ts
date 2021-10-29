a7dd614c91243722bee08eb00b94668a954e13d5
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-29 16:08:38 +0200

    fix(caa upload): filter out 7digital placeholders

--- src/mb_enhanced_cover_art_uploads/providers/7digital.ts
+++ src/mb_enhanced_cover_art_uploads/providers/7digital.ts
@@ -1,4 +1,6 @@
 import { HeadMetaPropertyProvider } from './base';
+import type { CoverArt } from './base';
+import type { FetchedImage } from '../fetch';
 
 // Technically, the cover URL is very predictable from the release ID. However,
 // we can also grab it from the <head> element metadata, which is a lot less
@@ -8,4 +10,12 @@ export class SevenDigitalProvider extends HeadMetaPropertyProvider {
     favicon = 'https://css-cdn.7digital.com/static/build/images/favicons/7digital/touch-ipad-retina.png';
     name = '7digital';
     urlRegex = /release\/.*-(\d+)(?:\/|$)/;
+
+    override postprocessImages(images: Array<[CoverArt, FetchedImage]>): Promise<FetchedImage[]> {
+        return Promise.resolve(images
+            // Filter out images that either are, or were redirected to the cover
+            // with ID 0000000016. This is a placeholder image.
+            .filter((pair) => !/\/0000000016_\d+/.test(pair[1].fetchedUrl.pathname))
+            .map((pair) => pair[1]));
+    }
 }
