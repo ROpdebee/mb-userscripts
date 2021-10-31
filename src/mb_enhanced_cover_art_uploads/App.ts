acad0d0b57169791521589bacc64b1b623b86b72
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-10-31 13:25:31 +0100

    refactor(caa upload): invert control between UI and App
    Previously, App called into UI, now, UI will call into App. This
    will make it somewhat easier to allow the user to configure certain
    parameters (such as fetching only the first image) through the UI.
    Without this change, the app would need to provide a callback to the
    UI to be fired whenever the configuration state changes, now, the UI
    can call the appropriate methods itself.
    
    The main user-visible change is that this will now lead to the input
    being cleared regardless of whether the queueing was successful.
    However, it makes sense to clear the input automatically even on
    failure to allow trying again with the same or a different URL.
    Moreover, this fixes an issue where input wasn't cleared when
    multiple URLs were entered, separated by whitespace.
    
    We're also moving the App class to a separate file, mainly because
    otherwise we need to export it from `index.ts` which leads to
    unnecessary warnings when building. It also cleans up the imports
    in `index.ts` as an added benefit.

---
+++ src/mb_enhanced_cover_art_uploads/App.ts
@@ -0,0 +1,119 @@
+import { EditNote } from '@lib/MB/EditNote';
+import { assertHasValue } from '@lib/util/assert';
+import { LOGGER } from '@lib/logging/logger';
+import { getProvider } from './providers';
+import type { CoverArt } from './providers/base';
+import { StatusBanner } from './ui/status_banner';
+import { InputForm } from './ui/main';
+import type { FetchedImages } from './fetch';
+import { ImageFetcher } from './fetch';
+import { SeedParameters } from './seeding/parameters';
+import { enqueueImages, fillEditNote } from './form';
+import { getURLsForRelease } from '@lib/MB/URLs';
+
+export class App {
+    #note: EditNote;
+    #fetcher: ImageFetcher;
+    #ui: InputForm;
+    #urlsInProgress: Set<string>;
+
+    constructor() {
+        this.#note = EditNote.withFooterFromGMInfo();
+        this.#fetcher = new ImageFetcher();
+        this.#urlsInProgress = new Set();
+
+        const banner = new StatusBanner();
+        LOGGER.addSink(banner);
+        this.#ui = new InputForm(banner.htmlElement, this);
+    }
+
+    async processURL(url: URL): Promise<void> {
+        // Don't process a URL if we're already doing so
+        if (this.#urlsInProgress.has(url.href)) {
+            return;
+        }
+
+        try {
+            this.#urlsInProgress.add(url.href);
+            await this.#_processURL(url);
+        } finally {
+            this.#urlsInProgress.delete(url.href);
+        }
+    }
+
+    async #_processURL(url: URL): Promise<void> {
+        // eslint-disable-next-line init-declarations
+        let fetchResult: FetchedImages;
+        try {
+            fetchResult = await this.#fetcher.fetchImages(url, /* TODO: Make configurable */ false);
+        } catch (err) {
+            LOGGER.error('Failed to grab images', err);
+            return;
+        }
+
+        try {
+            await enqueueImages(fetchResult);
+        } catch (err) {
+            LOGGER.error('Failed to enqueue images', err);
+            return;
+        }
+
+        fillEditNote([fetchResult], '', this.#note);
+        if (fetchResult.images.length) {
+            LOGGER.success(`Successfully added ${fetchResult.images.length} image(s)`);
+        }
+    }
+
+    async processSeedingParameters(): Promise<void> {
+        const params = SeedParameters.decode(new URLSearchParams(document.location.search));
+        // Although this is very similar to `processURL`, we may have to fetch
+        // and enqueue multiple images. We want to fetch images in parallel, but
+        // enqueue them sequentially to ensure the order stays consistent.
+
+        // eslint-disable-next-line init-declarations
+        let fetchResults: Array<[FetchedImages, CoverArt]>;
+        try {
+            fetchResults = await Promise.all(params.images
+                .map(async (cover): Promise<[FetchedImages, CoverArt]> => {
+                    return [await this.#fetcher.fetchImages(cover.url, /* TODO: Make configurable */ false), cover];
+                }));
+        } catch (err) {
+            LOGGER.error('Failed to grab images', err);
+            return;
+        }
+
+        // Not using Promise.all to ensure images get added in order.
+        for (const [fetchResult, cover] of fetchResults) {
+            try {
+                await enqueueImages(fetchResult, cover.types, cover.comment);
+            } catch (err) {
+                LOGGER.error('Failed to enqueue some images', err);
+            }
+        }
+
+        fillEditNote(fetchResults.map((pair) => pair[0]), params.origin ?? '', this.#note);
+        const totalNumImages = fetchResults.reduce((acc, pair) => acc + pair[0].images.length, 0);
+        if (totalNumImages) {
+            LOGGER.success(`Successfully added ${totalNumImages} image(s)`);
+        }
+    }
+
+    async addImportButtons(): Promise<void> {
+        const mbid = window.location.href.match(/musicbrainz\.org\/release\/([a-f0-9-]+)\//)?.[1];
+        assertHasValue(mbid);
+        const attachedURLs = await getURLsForRelease(mbid, {
+            excludeEnded: true,
+            excludeDuplicates: true,
+        });
+        const supportedURLs = attachedURLs.filter((url) => getProvider(url)?.allowButtons);
+
+        if (!supportedURLs.length)
+            return;
+
+        supportedURLs.forEach((url) => {
+            const provider = getProvider(url);
+            assertHasValue(provider);
+            this.#ui.addImportButton(this.processURL.bind(this, url), url.href, provider);
+        });
+    }
+}
