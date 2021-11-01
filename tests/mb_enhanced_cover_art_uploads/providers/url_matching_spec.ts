b259d03be6e5eba8690cdf677a0d0a3acbda20e5
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-11-01 16:59:09 +0100

    test(caa upload): fix test naming in shared URL matching spec

--- tests/mb_enhanced_cover_art_uploads/providers/url_matching_spec.ts
+++ tests/mb_enhanced_cover_art_uploads/providers/url_matching_spec.ts
@@ -39,7 +39,7 @@ export const urlMatchingSpec = ({ provider, supportedUrls, unsupportedUrls }: Sp
     // This test has two purposes:
     //   1. Additional testing of DispatchMap
     //   2. Making sure we don't forget to register a provider.
-    it.each(supportedUrls)('can find the provider from the dispatch map', ({ url }) => {
+    it.each(supportedUrls)('can find the provider in the dispatch map for $desc', ({ url }) => {
         // Expect them to be of the same class. We can't expect them to be
         // the exact same instance because the test suite that uses this
         // shared spec may create a whole other instance.
@@ -47,7 +47,7 @@ export const urlMatchingSpec = ({ provider, supportedUrls, unsupportedUrls }: Sp
             .toBe(provider.constructor);
     });
 
-    it.each(unsupportedUrls)('does not return provider for unsupported URL', ({ url }) => {
+    it.each(unsupportedUrls)('does not return provider from the dispatch map for $desc', ({ url }) => {
         expect(getProvider(new URL(url)))
             .toBeUndefined();
     });
