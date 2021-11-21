301a6e4beea332f4a0b4d71ed035cc418e9e2aae
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-11-21 23:53:48 +0100

    build: transpile async to promises instead of regenerator
    Leads to massive reduction in code size and may eliminate the need
    for regenerator-runtime in other scripts. ECAU still needs regenerator
    since it uses async generators, which aren't supported by this plugin.

--- babel.config.cjs
+++ babel.config.cjs
@@ -14,12 +14,20 @@ const prodConfig = {
             allExtensions: true,
         }],
         ['@babel/preset-env', {
-            corejs: '3.13',
+            corejs: '3.19',
             useBuiltIns: 'entry',
         }],
     ],
     plugins: [
         '@babel/plugin-syntax-jsx',
+        // Transform async/await into promises. Need to do this before plugin-transform-runtime
+        // as that will transpile to generators with regenerator. This will skip
+        // complex async things, like async generators, those will still be handled
+        // by babel itself. However, transforming "simple" async/await to promises
+        // leads to a severely smaller footprint in the output.
+        ['babel-plugin-transform-async-to-promises', {
+            externalHelpers: true,
+        }],
         ['@babel/plugin-transform-runtime', {
             regenerator: true,
             helpers: false,
