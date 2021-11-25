c5b2994c496a74dba96ce89a13fd9c41fac766e5
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-11-25 19:13:18 +0100

    fix(build): exclude asyncIterator from polyfills
    This leads to conflicts if multiple scripts load the polyfill on
    older browsers, as the second script will try to write a read-only
    property which was already set by the first script. We need a better
    strategy to deal with such conflicts though...

--- build/plugin-userscript.ts
+++ build/plugin-userscript.ts
@@ -145,7 +145,7 @@ export class MetadataGenerator {
         // TODO: This URL is versioned to allow updates later on without userscript
         // engines reusing an older version. However, how will we handle updates?
         const requires: string[] = Array.isArray(allMetadata.require) ? allMetadata.require : filterNonNull([allMetadata.require as string]);
-        const polyfillUrl = 'https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2018%2Ces2019%2Ces5%2Ces6%2Ces7%2Ces2021&flags=gated&version=3.108.0';
+        const polyfillUrl = 'https://polyfill.io/v3/polyfill.min.js?features=default%2Ces2015%2Ces2016%2Ces2017%2Ces2019%2Ces5%2Ces6%2Ces7%2Ces2021&flags=gated&version=3.108.0';
         requires.push(polyfillUrl);
         allMetadata.require = requires;
 
