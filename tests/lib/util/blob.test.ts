a690d850a954b5ae373a0eaf3a7c60e4f83cc242
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2021-12-11 21:47:57 +0100

    test(lib): fix test after crypto polyfill

--- tests/lib/util/blob.test.ts
+++ tests/lib/util/blob.test.ts
@@ -3,8 +3,9 @@ import { blobToDigest } from '@lib/util/blob';
 describe('converting blob to digest', () => {
     it('returns digest of blob', async () => {
         await expect(blobToDigest(new Blob(['test'])))
-            // in node, this will return the hex content of the response, not a SHA-256
-            // digest, since the SubtleCrypto API isn't available.
-            .resolves.toBe('74657374');
+            // Normally, in node, this will return the hex content of the
+            // response, not a SHA-256 digest, since the SubtleCrypto API
+            // isn't available. However, we're polyfilling it in tests/util/fix-jsdom-env.ts
+            .resolves.toBe('9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08');
     });
 });
