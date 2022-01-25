f9b4e93edca3f70b9fe81933a2e38b4f1702b7f9
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2022-01-25 14:04:43 +0100

    feat(ecau): add CAA provider
    This just specialises MB and delegates to IA, but this would allow
    someone to paste in a CAA release URL. Direct image URLs are ignored
    here, on purpose.

--- src/mb_enhanced_cover_art_uploads/providers/musicbrainz.ts
+++ src/mb_enhanced_cover_art_uploads/providers/musicbrainz.ts
@@ -9,7 +9,7 @@ export class MusicBrainzProvider extends CoverArtProvider {
     favicon = 'https://musicbrainz.org/static/images/favicons/favicon-32x32.png';
     override allowButtons = false;
     name = 'MusicBrainz';
-    urlRegex = /release\/([a-z0-9-]+)/;
+    urlRegex = /release\/([a-f0-9-]+)/;
 
     async findImages(url: URL): Promise<CoverArt[]> {
         const mbid = this.extractId(url);
@@ -17,4 +17,12 @@ export class MusicBrainzProvider extends CoverArtProvider {
         // Delegate to Archive.org provider to prevent useless redirects.
         return new ArchiveProvider().findImagesCAA(`mbid-${mbid}`);
     }
+}
+
+// Mostly identical to MB provider, but a different domain and name.
+export class CoverArtArchiveProvider extends MusicBrainzProvider {
+    override supportedDomains = ['coverartarchive.org'];
+    override favicon = 'https://coverartarchive.org/favicon.png';
+    override name = 'CoverArtArchive';
+    override urlRegex = /release\/([a-f0-9-]+)\/?$/; // Don't match direct image URLs!
 }
