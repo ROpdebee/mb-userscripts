3c706b1cbcf014147f1919d050a29bce9ce1976e
David Kellner <52860029+kellnerd@users.noreply.github.com>
2022-01-27 15:57:50 +0100

    style(util): remove linter exception

--- src/lib/util/urls.ts
+++ src/lib/util/urls.ts
@@ -2,8 +2,7 @@ export function urlBasename(url: string | URL, defaultBasename = ''): string {
     if (typeof url !== 'string') url = url.pathname;
     // We don't need nullish coalescing here, since the array will always have
     // at least one element, but the last element may be the empty string.
-    // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
-    return url.split('/').pop() || defaultBasename;
+    return url.split('/').pop()! || defaultBasename;
 }
 
 export function urlJoin(base: string | URL, sub: string): URL {
