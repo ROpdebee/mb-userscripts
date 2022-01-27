60624a48a8d90ef0b5ececd27ad6c81d8e48e3c9
Ruben Opdebeeck <15186467+ROpdebee@users.noreply.github.com>
2022-01-27 14:48:13 +0100

    refactor(ecau): more elegant URL joining
    Co-authored-by: David Kellner <52860029+kellnerd@users.noreply.github.com>

--- src/lib/util/urls.ts
+++ src/lib/util/urls.ts
@@ -1,5 +1,3 @@
-import { assert } from './assert';
-
 export function urlBasename(url: string | URL, defaultBasename = ''): string {
     if (typeof url !== 'string') url = url.pathname;
     // We don't need nullish coalescing here, since the array will always have
@@ -9,10 +7,9 @@ export function urlBasename(url: string | URL, defaultBasename = ''): string {
 }
 
 export function urlJoin(base: string | URL, ...subPaths: string[]): URL {
-    assert(subPaths.length >= 1);
-    let newUrl = new URL(subPaths[0], base);
-    for (const nextSub of subPaths.slice(1)) {
-        newUrl = new URL(nextSub, newUrl);
+    let newUrl = new URL(base);
+    for (const subPath of subPaths) {
+        newUrl = new URL(subPath, newUrl);
     }
     return newUrl;
 }
