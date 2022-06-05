f6dd57e4ff9d521ca9f916b63225367be4aceaa4
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2022-06-05 13:35:38 +0200

    fix(lib): proper typing for memoization function
    Previous type signature didn't retain the wrapped function's signature.

--- src/lib/util/functions.ts
+++ src/lib/util/functions.ts
@@ -6,15 +6,15 @@
  * when a subsequent evaluation of the function can reuse an old value, and
  * which old application can be reused.
  *
- * @param      {(...args:T[])=>V}       fn      The function to memoize.
- * @param      {(...args:T[])=>string}  keyFn   Function to extract a
+ * @param      {(...args:T)=>V}       fn      The function to memoize.
+ * @param      {(...args:T)=>string}  keyFn   Function to extract a
  *                                              memoization key.
- * @return     {(...args:T[])=>V}       Memoized version of `fn`.
+ * @return     {(...args:T)=>V}       Memoized version of `fn`.
  */
-export function memoize<T, V>(fn: (...args: T[]) => V, keyFn: (args: T[]) => string = (args): string => `${args[0]}`): (...args: T[]) => V {
+export function memoize<T extends unknown[], V>(fn: (...args: T) => V, keyFn: (args: T) => string = (args): string => `${args[0]}`): (...args: T) => V {
     const memoMap = new Map<string, V>();
 
-    return (...args: T[]): V => {
+    return (...args: T): V => {
         // We're requiring the memoization key to be a string to ensure proper
         // equality checks. Not the best solution, but it works.
         const key = keyFn(args);
