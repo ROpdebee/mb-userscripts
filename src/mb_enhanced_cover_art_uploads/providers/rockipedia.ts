6c87f3fcf56c6fdb7f9d5736d0796b5ffa56cb97
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2022-06-07 15:01:50 +0200

    feat(ecau): rockipedia.no provider

---
+++ src/mb_enhanced_cover_art_uploads/providers/rockipedia.ts
@@ -0,0 +1,42 @@
+import { LOGGER } from '@lib/logging/logger';
+import { filterNonNull } from '@lib/util/array';
+import { assertDefined } from '@lib/util/assert';
+import { parseDOM, qsa } from '@lib/util/dom';
+
+import type { CoverArt } from './base';
+import { CoverArtProvider } from './base';
+
+// There's an API, but it requires an API key.
+export class RockipediaProvider extends CoverArtProvider {
+    supportedDomains = ['rockipedia.no'];
+    favicon = 'https://www.rockipedia.no/wp-content/themes/rockipedia/img/favicon.ico';
+    name = 'Rockipedia';
+    urlRegex = /utgivelser\/.+?-(\d+)/;
+
+    async findImages(url: URL): Promise<CoverArt[]> {
+        const id = this.extractId(url);
+        assertDefined(id);
+        const imageBrowserUrl = new URL(`https://www.rockipedia.no/?imagebrowser=true&t=album&id=${id}`);
+        const imageBrowserDoc = parseDOM(await this.fetchPage(imageBrowserUrl), url.href);
+
+        // No failure case, even for wrong IDs it still returns an empty gallery.
+
+        const coverElements = qsa<HTMLLIElement>('li.royalSlide', imageBrowserDoc);
+        return filterNonNull(coverElements.map((coverElement) => {
+            const coverUrl = coverElement.getAttribute('data-src');
+            // istanbul ignore if: Should not happen
+            if (coverUrl === null) {
+                LOGGER.warn(`Could not extract a cover for Rockipedia release ${url}: Unexpected null src`);
+                return null;
+            }
+
+            return {
+                url: new URL(coverUrl),
+                // No types. Sometimes there's a caption "Omslagsfoto", which
+                // translates to "cover photo", but I don't think that necessarily
+                // means "front cover". See e.g. https://www.rockipedia.no/utgivelser/good_man_good_girl__(radio_edit)-25786/,
+                // that's a picture of the medium.
+            };
+        }));
+    }
+}
