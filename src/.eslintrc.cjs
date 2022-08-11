c9fc90ead6277d7a5143ffa24b9c9cf33ff0c338
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2022-08-11 21:46:02 +0200

    refactor(lib): remove xhr wrapper, force usage of unified interface

--- src/.eslintrc.cjs
+++ src/.eslintrc.cjs
@@ -7,7 +7,8 @@ module.exports = {
     },
     rules: {
         // Also restrict usage of the GM object, we should always go through the
-        // compat layer.
-        'no-restricted-globals': ['error', 'origin', 'GM'].concat(restrictedGlobals),
+        // compat layer. Restrict the usage of `fetch` too, we should use the
+        // unified request interface instead.
+        'no-restricted-globals': ['error', 'origin', 'GM', 'fetch'].concat(restrictedGlobals),
     },
 };
