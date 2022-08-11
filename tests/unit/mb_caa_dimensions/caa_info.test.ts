d38be336c366070fe088d7e4e9eb411a6b4bb84a
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2022-08-11 21:41:37 +0200

    refactor(caa dims): switch to unified request interface

--- tests/unit/mb_caa_dimensions/caa_info.test.ts
+++ tests/unit/mb_caa_dimensions/caa_info.test.ts
@@ -1,16 +1,8 @@
-import NodeHttpAdapter from '@pollyjs/adapter-node-http';
-
 import { getCAAInfo } from '@src/mb_caa_dimensions/caa_info';
-import { mockFetch, setupPolly } from '@test-utils/pollyjs';
+import { setupPolly } from '@test-utils/pollyjs';
 
 // eslint-disable-next-line jest/require-hook
-setupPolly({
-    adapters: [NodeHttpAdapter],
-});
-
-beforeAll(() => {
-    mockFetch();
-});
+setupPolly();
 
 describe('getting CAA information', () => {
     it('returns size and format', async () => {
