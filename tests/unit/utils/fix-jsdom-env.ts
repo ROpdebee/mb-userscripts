67159d7011c673d505195fe61ca49cf16f3e0fc0
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2023-06-08 15:32:55 -0400

    test: fix crypto.subtle.digest errors on newer Node versions
    Seems like a recent Node version has started to do stricter checks
    on the data passed into the webcrypto APIs. In lib/util/blob.ts, we're
    passing in the result of a FileReader, which is polyfilled by jest and
    is using a fake ArrayBuffer, which fails this check. Therefore, we need
    to convert this fake ArrayBuffer into a valid Node object before passing
    it to the API.

--- tests/unit/utils/fix-jsdom-env.ts
+++ tests/unit/utils/fix-jsdom-env.ts
@@ -6,5 +6,16 @@ global.TextEncoder = TextEncoder;
 
 // Same for crypto
 import { webcrypto } from 'node:crypto';
+const cryptoDigest = webcrypto.subtle.digest.bind(webcrypto.subtle);
 // @ts-expect-error: Some incompatibilities between node and web.
 crypto.subtle = webcrypto.subtle;
+crypto.subtle.digest = (algorithm, data): Promise<ArrayBuffer> => {
+    if (data instanceof ArrayBuffer) {
+        // Need to copy the given data, otherwise node's webcrypto digest function
+        // throws an error stating that the data isn't an ArrayBuffer. This
+        // probably has something to do with the FileReader that's polyfilled by
+        // jest, used in lib/util/blob.
+        data = new Uint8Array(data);
+    }
+    return cryptoDigest(algorithm, data);
+};
