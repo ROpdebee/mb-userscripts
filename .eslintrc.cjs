2ee46bce4a66ffa03f63930b217014c0e13dbebc
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2023-08-02 16:57:48 -0400

    style: switch to new typescript-eslint configs
    We'll use the stricter rules from now on too, and enable new stylistic
    configurations.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,8 +15,8 @@ module.exports = {
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
         'plugin:eslint-comments/recommended',
         'adjunct',
     ],
