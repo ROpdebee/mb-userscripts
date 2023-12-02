09a4c124fa7981cbe974d868be4b0eafe212b972
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2023-12-02 12:20:57 +0100

    build: let babel transpile async functions again
    We previously used a plugin that transformed async functions to
    promises. However, we likely don't need to transpile functions
    anymore because of the browser updates, so we can remove this
    exception and let babel do the transpilation itself.

--- babel.config.cjs
+++ babel.config.cjs
@@ -16,24 +16,10 @@ const prodConfig = {
         ['@babel/preset-env', {
             corejs: '3.19',
             useBuiltIns: 'entry',
-            exclude: [
-                // Don't transform async functions and async generators using
-                // babel itself, this is handled by a plugin.
-                '@babel/plugin-transform-async-to-generator',
-                '@babel/plugin-proposal-async-generator-functions',
-            ],
         }],
     ],
     plugins: [
         '@babel/plugin-syntax-jsx',
-        // Transform async/await into promises. Need to do this before plugin-transform-runtime
-        // as that will transpile to generators with regenerator. This will skip
-        // complex async things, like async generators, those will still be handled
-        // by babel itself. However, transforming "simple" async/await to promises
-        // leads to a severely smaller footprint in the output.
-        ['babel-plugin-transform-async-to-promises', {
-            externalHelpers: true,
-        }],
         ['@babel/plugin-transform-runtime', {
             regenerator: true,
             helpers: false,
