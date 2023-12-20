a725f617a5e8f923356ec2596ad59a12b8ed1173
ROpdebee <15186467+ROpdebee@users.noreply.github.com>
2023-12-20 13:04:51 +0100

    feat(ecau): add YouTube provider
    Test case for 404 releases is currently disabled because the test
    bumps into a consent page.

---
+++ src/mb_enhanced_cover_art_uploads/providers/youtube.ts
@@ -0,0 +1,29 @@
+import type { RequestOptions } from '@lib/util/request';
+
+import { HeadMetaPropertyProvider } from './base';
+
+export class YoutubeProvider extends HeadMetaPropertyProvider {
+    public readonly supportedDomains = ['youtube.com'];
+    public readonly favicon = 'https://www.youtube.com/s/desktop/e4d15d2c/img/favicon_144x144.png';
+    public readonly name = 'YouTube';
+    protected readonly urlRegex = /watch\?v=(\w+)/;
+
+    protected override cleanUrl(url: URL): string {
+        return super.cleanUrl(url) + url.search;
+    }
+
+    protected override is404Page(doc: Document): boolean {
+        return doc.body.innerHTML.includes("This video isn't available anymore");
+    }
+
+    protected override fetchPage(url: URL, options?: RequestOptions | undefined): Promise<string> {
+        // Override to add a language header, otherwise the 404 check will break depending on countries.
+        return super.fetchPage(url, {
+            ...options,
+            headers: {
+                ...options?.headers,
+                'Accept-Language': 'en-GB,en;q=0.5',
+            },
+        });
+    }
+}
---
+++ tests/unit/mb_enhanced_cover_art_uploads/providers/youtube.test.ts
@@ -0,0 +1,51 @@
+import { ArtworkTypeIDs } from '@lib/MB/CoverArt';
+import { YoutubeProvider } from '@src/mb_enhanced_cover_art_uploads/providers/youtube';
+import { itBehavesLike } from '@test-utils/shared_behaviour';
+
+import { findImagesSpec } from './find_images_spec';
+import { urlMatchingSpec } from './url_matching_spec';
+
+describe('youtube provider', () => {
+    const provider = new YoutubeProvider();
+
+    describe('url matching', () => {
+        const supportedUrls = [{
+            desc: 'video URLs',
+            url: 'https://www.youtube.com/watch?v=Py21QCndbxc',
+            id: 'Py21QCndbxc',
+        }];
+
+        const unsupportedUrls = [{
+            desc: 'channel URLs',
+            url: 'https://www.youtube.com/@NanashiMumei',
+        }];
+
+        // eslint-disable-next-line jest/require-hook
+        itBehavesLike(urlMatchingSpec, { provider, supportedUrls, unsupportedUrls });
+    });
+
+    describe('extracting images', () => {
+        const extractionCases = [{
+            desc: 'music video',
+            url: 'https://www.youtube.com/watch?v=Py21QCndbxc',
+            numImages: 1,
+            expectedImages: [{
+                index: 0,
+                urlPart: 'Py21QCndbxc/maxresdefault',
+                types: [ArtworkTypeIDs.Front],
+            }],
+        }];
+
+        // Failure case test doesn't currently work because YouTube seems to redirect
+        // to a consent page on missing releases, which blocks the test.
+        /*
+        const extractionFailedCases = [{
+            desc: 'non-existent video',
+            url: 'https://www.youtube.com/watch?v=py21QCndbXc',
+        }];
+        */
+
+        // eslint-disable-next-line jest/require-hook
+        itBehavesLike(findImagesSpec, { provider, extractionCases, extractionFailedCases: [] });
+    });
+});
